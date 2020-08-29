5a4aec3170af3b1a2b22f3e8b736be98c72ca0c1
Bogdan <bladeinnfs@gmail.com>
2020-08-29 19:55:28 +0700

    completed

--- solution.py
+++ solution.py
@@ -8,7 +8,10 @@ class CarBase:
     def __init__(self, brand, photo_file_name, carrying):
         self.brand = brand
         self.photo_file_name = photo_file_name
-        self.carrying = carrying
+        try:
+            self.carrying = float(carrying)
+        except ValueError:
+            self.carrying = 0.0
 
     def get_photo_file_ext(self):
         return os.path.splitext(self.photo_file_name)[1]
@@ -16,11 +19,14 @@ class CarBase:
 
 class Car(CarBase):
     car_type = "car"
-    passenger_seats_count = 2
+    passenger_seats_count = 0
 
-    def __init__(self, brand, passenger_seats_count, photo_file_name, carrying,):
+    def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
         super().__init__(brand, photo_file_name, carrying)
-        self.passenger_seats_count = passenger_seats_count
+        try:
+            self.passenger_seats_count = int(passenger_seats_count)
+        except ValueError:
+            self.passenger_seats_count = 0
 
 
 class Truck(CarBase):
@@ -33,13 +39,13 @@ class Truck(CarBase):
             try:
                 body_lst = list(map(float, body_lst))
             except ValueError:
-                self.body_length = self.body_width = self.body_height = 0
+                self.body_length = self.body_width = self.body_height = 0.0
             else:
                 self.body_length = body_lst[0]
                 self.body_width = body_lst[1]
                 self.body_height = body_lst[2]
         else:
-            self.body_length = self.body_width = self.body_height = 0
+            self.body_length = self.body_width = self.body_height = 0.0
 
     def get_body_volume(self):
         return self.body_length * self.body_width * self.body_height
@@ -47,7 +53,6 @@ class Truck(CarBase):
 
 class SpecMachine(CarBase):
     car_type = "spec_machine"
-    extra = ""
 
     def __init__(self, brand, photo_file_name, carrying, extra):
         super().__init__(brand, photo_file_name, carrying)
@@ -60,21 +65,60 @@ def get_car_list(csv_filename):
         reader = csv.reader(f)
         next(reader)
         for row in reader:
-            for elem in row:
-                elems = elem.split(";")
-            if elems[0] == "car":
-                tmp_obj = Car(elems[1], elems[2], elems[3], elems[5])
-                car_list.append(tmp_obj)
+            elem = row[0].split(";")
+            if len(elem) < 7:
+                continue
+            car_type = elem[0]
+            brand = elem[1]
+            passenger_seats_count = elem[2]
+            photo_file_name = elem[3]
+            body_whl = elem[4]
+            print(f"body whl = {body_whl}, type {type(body_whl)}")
+            carrying = elem[5]
+            extra = elem[6]
+            if (car_type not in ["car", "truck", "spec_machine"]) or (brand is "") or (carrying is "") or (
+                    photo_file_name is ""):
+                continue
+            try:
+                float(carrying)
+            except ValueError:
+                continue
 
+            if os.path.splitext(photo_file_name)[1] not in ['.jpg', '.jpeg', '.png', '.gif']:
+                continue
+
+            if car_type == "car":
+                if passenger_seats_count is "":
+                    continue
+                try:
+                    passenger_seats_count = int(passenger_seats_count)
+                except ValueError:
+                    continue
+                tmp_obj = Car(brand, photo_file_name, carrying, passenger_seats_count)
+                car_list.append(tmp_obj)
+            elif car_type == "truck":
+                tmp_obj = Truck(brand, photo_file_name, carrying, body_whl)
+                car_list.append(tmp_obj)
+            elif car_type == "spec_machine":
+                if extra is "":
+                    continue
+                tmp_obj = SpecMachine(brand, photo_file_name, carrying, extra)
+                car_list.append(tmp_obj)
         return car_list
 
 
 if __name__ == "__main__":
     result = get_car_list("test.csv")
     for elem in result:
-        print(elem.car_type)
-        print(elem.brand)
-        print(elem.passenger_seats_count)
-        print(elem.photo_file_name)
-        print(elem.carrying)
+        print(f"Car type: {elem.car_type}")
+        print(f"Brand: {elem.brand}")
+        print(f"Carrying: {elem.carrying}")
+        if elem.car_type == "car":
+            print(f"Passenger seat count: {elem.passenger_seats_count}")
+        elif elem.car_type == "truck":
+            print(f"Body whl: {elem.body_width} x {elem.body_height} x {elem.body_length}")
+        elif elem.car_type == "spec_machine":
+            print(f"Extra: {elem.extra}")
+        print(f"Photo: {elem.photo_file_name}")
         print("------------------------")
+    print(f"Total units: {len(result)}")
